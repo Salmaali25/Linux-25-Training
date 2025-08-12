de9a7028959f23699b1d2df2edc4dbcd8dfe3b0e
salmaali25 <salmaali098.mohamed@gmail.com>
2025-08-12 06:50:21 -0400

    update solution.sh

--- Task-3/solution.sh
+++ Task-3/solution.sh
@@ -1,1 +1,15 @@
-
+sudo useradd -m hero
+sudo useradd intruder
+sudo userdel -r intruder
+sudo groupadd warriors
+sudo groupadd healers
+sudo usermod -aG warriors hero
+sudo useradd sage
+sudo usermod -aG healers sage
+echo "cyberworld's core" > treasure.txt
+chmod 000 treasure.txt
+sudo chown hero treasure.txt
+sudo chmod 600 treasure.txt
+sleep 100 &
+ps
+kill -19 19567
